739d448eb9ce3eef8fd2fb6c5f79b74474186b6d
Sergey <is_everything@mail.ru>
2014-10-14 17:46:36 +0600

    Мелочи в js

--- js/common.js
+++ js/common.js
@@ -1,19 +1,26 @@
-String.prototype.trim = function () {
-    var i = 0;
-    while (i < this.length && (this.charAt(i) == " " || this.charAt(i) == "\n" || this.charAt(i) == "\r" || this.charAt(i) == "\t")) i++;
-    var j = this.length - 1;
-    while (j >= 0 && (this.charAt(j) == " " || this.charAt(j) == "\n" || this.charAt(j) == "\r" || this.charAt(j) == "\t")) j--;
-    return this.substr(i, j - i + 1);
+if (typeof String.prototype.trim === 'undefined') {
+    String.prototype.trim = function () {
+        var i = 0;
+        while (i < this.length && (this.charAt(i) == " " || this.charAt(i) == "\n" || this.charAt(i) == "\r" || this.charAt(i) == "\t")) i++;
+        var j = this.length - 1;
+        while (j >= 0 && (this.charAt(j) == " " || this.charAt(j) == "\n" || this.charAt(j) == "\r" || this.charAt(j) == "\t")) j--;
+        return this.substr(i, j - i + 1);
+    };
 }
-Array.prototype.indexOf = function (v) {
-    for (var i = 0; i < this.length; i++)
-        if (this[i] == v)
-            return i;
-    return -1;
+
+if (typeof Array.prototype.indexOf === 'undefined') {
+    Array.prototype.indexOf = function (v) {
+        for (var i = 0; i < this.length; i++)
+            if (this[i] == v)
+                return i;
+        return -1;
+    };
 }
+
 function wrapAs(s, c) {
     return "<span class='" + c + "'>" + s + "</span>";
 }
+
 function ge(e) {
     return document.getElementById(e);
 }
@@ -23,7 +30,7 @@ function computedHeight(e) {
 }
 
 function bodyMargin() {
-    var style = getComputedStyle(document.body, "")
+    var style = getComputedStyle(document.body, "");
     return parseInt(style.getPropertyValue("margin-top")) +
         parseInt(style.getPropertyValue("margin-bottom"));
 }
